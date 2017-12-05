bd6acb59f1ffb29a743a3ff927244a721cc856a9
Tomohiro I <itomomoti@gmail.com>
2017-12-05 21:20:12 +0900

    include TagRelabelAlgo in namespace itmmti, update CMakeLists

--- TagRelabelAlgo.cpp
+++ TagRelabelAlgo.cpp
@@ -7,12 +7,15 @@
 
 #include "TagRelabelAlgo.hpp"
 
-const uint64_t TagRelabelAlgo::TBL_Capacities[] = {
-  9755,
-  4281003,
-  1196954387,
-  301970036317,
-  35278131863618,
-  3480544397494665,
-  216999516552746648,
-};
+namespace itmmti
+{
+  const uint64_t TagRelabelAlgo::TBL_Capacities[] = {
+    9755,
+    4281003,
+    1196954387,
+    301970036317,
+    35278131863618,
+    3480544397494665,
+    216999516552746648,
+  };
+}
